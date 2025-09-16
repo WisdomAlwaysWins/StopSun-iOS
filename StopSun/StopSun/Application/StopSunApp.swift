e91e585a602e5220c08cc20018ce62fb22d75d04
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-09-16 15:44:53 +0900

    chore: project 초기 세팅

---
+++ StopSun/StopSun/Application/StopSunApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopSunApp.swift
+//  StopSun
+//
+//  Created by taeni on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct StopSunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StopSun/StopSunWatch Watch App/Application/StopSunWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopSunWatchApp.swift
+//  StopSunWatch Watch App
+//
+//  Created by taeni on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct StopSunWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
