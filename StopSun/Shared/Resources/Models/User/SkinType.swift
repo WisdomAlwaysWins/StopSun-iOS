54cc343378c26e62a67d5947ce617f668447ffbe
TAENI <52228832+delightPIP@users.noreply.github.com>
2026-01-21 23:13:06 +0900

    fix: SkinType enum 의 title, summary, description 등을 localizable 화

--- StopSun/Shared/Resources/Models/User/SkinType.swift
+++ StopSun/Shared/Resources/Models/User/SkinType.swift
@@ -15,35 +15,28 @@ enum SkinType: Int, Codable, CaseIterable, Identifiable {
     case type4
     case type5
     case type6
-    
+
     var id: Int { rawValue }
-    
-    var title: String {
-        "\(rawValue)형"
+
+    private var localizationKeyBase: String {
+        "skin.type\(rawValue)"
     }
-    
-    var summary: String {
-        switch self {
-        case .type1: "매우 하얀 피부, 주근깨 많음"
-        case .type2: "하얀 피부, 주근깨 있음"
-        case .type3: "약간 어두운 피부"
-        case .type4: "올리브톤 또는 황갈색 피부"
-        case .type5: "갈색 피부"
-        case .type6: "매우 어두운 갈색/검은 피부"
-        }
+
+    var titleKey: String {
+        "skin.type\(rawValue).title"
     }
-    
-    var description: String {
-        switch self {
-        case .type1: "항상 타거나 화상을 입음, 거의 태닝되지 않음"
-        case .type2: "쉽게 타고, 태닝이 어려움"
-        case .type3: "가끔 타지만 점진적으로 태닝됨"
-        case .type4: "거의 타지 않고 쉽게 태닝됨"
-        case .type5: "거의 타지 않고 매우 쉽게 태닝됨"
-        case .type6: "거의 타지 않음, 자연스럽게 어두운 색소"
-        }
+
+    var summaryKey: String {
+        "skin.type\(rawValue).summary"
     }
-    
+
+    var descriptionKey: String {
+        "skin.type\(rawValue).description"
+    }
+}
+
+extension SkinType {
+
     var color: Color {
         switch self {
         case .type1: .skintype00
@@ -54,19 +47,19 @@ enum SkinType: Int, Codable, CaseIterable, Identifiable {
         case .type6: .skintype05
         }
     }
-    
-    /// 피부 타입의 하루 최대 권장 MED 평균값 (단위: J/m^2)
+
+    /// 피부 타입의 하루 최대 권장 MED 평균값 (J/m²)
     var maxMED: Double {
         switch self {
-        case .type1: 150.0
-        case .type2: 300.0
-        case .type3: 400.0
-        case .type4: 500.0
-        case .type5: 700.0
-        case .type6: 1200.0
+        case .type1: 150
+        case .type2: 300
+        case .type3: 400
+        case .type4: 500
+        case .type5: 700
+        case .type6: 1200
         }
     }
-    
+
     var romanNumeral: String {
         switch self {
         case .type1: "I"
