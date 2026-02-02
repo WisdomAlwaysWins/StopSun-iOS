2e5e59de86b0c6cd521682f390be215f73abbc5a
J <knowisdom314@gmail.com>
2026-02-02 18:59:49 +0900

    feat: SEDCalculator Unit Test 추가
    - Unit Test 19개

---
+++ StopSun/StopSunTests/StopSunTests.swift
@@ -0,0 +1,16 @@
+//
+//  StopSunTests.swift
+//  StopSunTests
+//
+//  Created by J on 2/2/26.
+//
+
+import Testing
+
+struct StopSunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
